1a32006c7422624b4ad14a479793e2f328d9db23
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-06 09:40:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -98,5 +98,20 @@ end
 
 
 def get_average_age_for_season(data, season)
-  # code here
+all_cont =[]
+
+  data.each {|s,contestants| 
+    if s.to_s === season #puts "#{s} & #{contestants}"
+        contestants.each {|cont_a| 
+          cont_a.each {|val_type, val| puts "#{val_type} & #{val}"             
+              if val_type == :age
+              all_cont << val
+              end
+          }
+        }
+    end
+  }
+
+total = (all_cont.inject { |a, b| a.to_i + b.to_i}) / all_cont.length
+
 end
