e8abce72d18a95287424017639db4e82bf748478
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-06 08:53:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,33 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+winner =[]
+
+  data.each {|s,contestants| 
+    if s.to_s === season
+      puts "#{s} & #{contestants}"
+        contestants.each {|cont_a| 
+          cont_a.each {|val_type, val| 
+            if val === "Winner"             
+              winner << cont_a
+            end
+          }
+        }
+    end
+  }
+
+winner_name = []
+winner.each {|key| puts "#{key}" 
+  key.each { |type, value| 
+    if type.to_s == "name"
+      winner_name << value
+    end
+  } 
+}
+
+(winner_name[0].split(" "))[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+    
 end
 
 def count_contestants_by_hometown(data, hometown)
